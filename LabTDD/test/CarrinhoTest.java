e26a825ae8fcbe7afe5873f51233fff2a685d8a2
30961874 <30961874@090030-P14S13.macklabs.br>
2012-09-20 22:33:50 -0300

    Teste

--- LabTDD/test/CarrinhoTest.java
+++ LabTDD/test/CarrinhoTest.java
@@ -48,7 +48,6 @@ public class CarrinhoTest {
 		Produto original = new Produto("Java em 24 horas", 50.00);
 		carrinho.add(original);
 		Produto copia = new Produto("Java em 24 horas", 50.00);
-                
 		original = carrinho.menorProduto();
 		assertArrayEquals(new Object[] { original }, new Object[] { copia });
 	}
@@ -64,4 +63,47 @@ public class CarrinhoTest {
                 ArrayList<Produto> lista = new ArrayList<Produto>();
                 assertEquals(lista.getClass(), carrinho.getProdutos().getClass());
         }
+        
+        @Test
+    public void removerUmProduto() throws CarrinhoVazioExpected {
+        Produto p1 = new Produto("Produto1", 10.00);
+        Produto p2 = new Produto("Produto2", 20.00);
+        carrinho.add(p1);
+        carrinho.add(p2);
+
+        carrinho.remove(p2);
+
+        assertEquals(1, carrinho.getProdutos().size());
+    }
+
+    @Test
+    public void removerTodosOsProdutos() throws CarrinhoVazioExpected {
+        Produto p1 = new Produto("Produto1", 10.00);
+        Produto p2 = new Produto("Produto2", 20.00);
+        Produto p3 = new Produto("Produto3", 30.00);
+        carrinho.add(p1);
+        carrinho.add(p2);
+        carrinho.add(p3);
+        
+        carrinho.limpar();
+        
+        assertEquals(0, carrinho.getProdutos().size());
+    }
+    
+    @Test
+    public void calcularValorTotalDoCarrinho() throws CarrinhoVazioExpected {
+        Produto p1 = new Produto("Produto1", 20.00);
+        Produto p2 = new Produto("Produto2", 20.00);
+        Produto p3 = new Produto("Produto3", 30.00);
+        
+        carrinho.add(p1);
+        carrinho.add(p2);
+        carrinho.add(p3);
+        
+        double soma = carrinho.getPrecoTotal();
+        
+        assertEquals(70.0, soma, 0); 
+    }
+
 }
+
